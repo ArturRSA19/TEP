de0136364b0308261c3a455b78b7d82ad3eed12c
Artur Rodrigues <artur190503@outlook.com>
2023-06-27 20:32:27 -0300

    Lista 11 - Questão B

---
+++ Listas/Lista 11/B.py
@@ -0,0 +1,10 @@
+n = int(input())
+lista = []
+
+lista = input()
+metade = len(lista)//2
+string1 = lista[metade:]
+string2 = lista[:metade]
+
+if(string1 == string2): print('Yes') 
+else: (print('No'))
