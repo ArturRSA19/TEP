ddc066f81927af6111c2f962bab0d34c9a235661
Artur Rodrigues <artur190503@outlook.com>
2023-06-28 20:38:55 -0300

    Lista 11 - Questão C

---
+++ Listas/Lista 11/C.py
@@ -0,0 +1,16 @@
+dia = str(input())
+aux = 7
+if(dia == 'SUN'):
+    print(aux)
+elif(dia == 'MON'):
+    print(aux-1)
+elif(dia == 'TUE'):
+    print(aux-2)
+elif(dia == 'WED'):
+    print(aux-3)
+elif(dia == 'THU'):
+    print(aux-4)
+elif(dia == 'FRI'):
+    print(aux-5)
+elif(dia == 'SAT'):
+    print(aux-6)
