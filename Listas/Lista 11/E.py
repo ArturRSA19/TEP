eca8a6b0d2d609df364d74ae124eb6a34dfcf8f7
Artur Rodrigues <artur190503@outlook.com>
2023-06-29 20:29:04 -0300

    Lista 11 - Questão E - Precisa de ajustes

---
+++ Listas/Lista 11/E.py
@@ -0,0 +1,20 @@
+import math
+
+t = int(input())
+
+for i in range(t):
+    x, y = map(int, input().split())
+
+    aux = y - x # Valor necessário para chegar de x até y
+    resultado = math.ceil(abs(aux) / 10) # Número mínimo de movimentos necessários para alcançar o múltiplo de 10 mais próximo
+
+    if resultado > 0:
+        x += resultado * 10
+
+    x += aux
+
+    print(resultado)
+    aux = 0
+    resultado = 0
+
+
